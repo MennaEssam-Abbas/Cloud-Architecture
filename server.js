7d29156b026ff52310e0611a06548aebdaaada55
MennaEssam-Abbas <mennaessamabbas2@gmail.com>
2025-03-12 19:42:05 +0200

    Initial commit

--- server.js
+++ server.js
@@ -2,21 +2,28 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
-dotenv.config({path:"config.env"});
+dotenv.config({ path: 'config.env' });
 const dbConnection = require('./config/database');
 const categoryRoute = require('./routes/categoryRoute');
+
+// Connect with db
 dbConnection();
 
+// express app
 const app = express();
+
+// Middlewares
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+// Mount Routes
 app.use('/api/v1/categories', categoryRoute);
 
-app.get('/', (req, res) => {
-    res.send(' The First server is running');
-  });
-
-const PORT= process.env.PORT ;
-  app.listen(PORT, () => {
-    console.log(`The server is running on Port ${PORT}`);
-  });
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`App running running on port ${PORT}`);
+});
